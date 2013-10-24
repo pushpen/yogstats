b00f367772864bb7a8a1fc7e0df09d9e5ca0d391
Thomas Williams <t.r.williamst@googlemail.com>
2013-10-24 04:19:01 +0100

    API updates
    Made methods static, finished authentication method, added startSession
    method

--- PHP Lib/api.php
+++ PHP Lib/api.php
@@ -1,35 +1,115 @@
 <?php
 	require_once 'google-api-php-client/src/Google_Client.php';
-	//require_once 'google-api-php-client/src/contrib/
+	require_once 'google-api-php-client/src/contrib/Google_Oauth2Service.php';
 	
-	function hasSession()
+	class API
 	{
-		return isset(session_id() != '' && isset($_SESSION));
-	}
-	
-	function hasAuthed()
-	{
-		return isset($_SESSION['authState']);
-	}
-	
-	function authenticate()
-	{
-		if(!hasSession()) session_start();
-		if(!hasAuthed())
+		private static $clientID = '696232355614.apps.googleusercontent.com';
+		private static $clientSecret = '9hPHiYyVKNiMDNXkTNuayD6Q';
+		private static $redirectUri = 'http://localhost/yogstats/authCode.php';
+		
+		public static function hasSession()
 		{
-			$authState = md5(rand());
-			$_SESSION['authState'] = $authState;
+			return session_id() != '' && isset($_SESSION);
 		}
-	}
-	
-	function createAuthURI()
-	{
-		return "https://accounts.google.com/o/oauth2/auth?client_id=" + 
-		$clientID + 
-		"&response_type=code&scope=openid%20email&redirect_uri=" + 
-		$redirectURI +
-		"&state=security_token" + 
-		htmlentities($_SESSION['authState']) + 
-		%3D138r5719ru3e1%26url%3Dhttps://oa2cb.example.com/myHome&login_hint=jsmith@example.com";
+		
+		public static function hasCode()
+		{
+			return isset($_SESSION['authState']) && isset ($_SESSION['authCode']);
+		}
+		
+		public static function hasAccessCode()
+		{
+			return isset($_SESSION['authAccessToken']);
+		}
+		
+		public static function needsRefresh()
+		{
+			return !isset($GLOBALS['gClient']) || $GLOBALS['gClient']->isAccessTokenExpired();
+		}
+		
+		public static function startSession()
+		{
+			if(!API::hasSession()) session_start();
+		}
+		
+		//Permissions array optional, passing with no parameters will run default auth method
+		public static function authenticate($permissions = array('openid', 'email'))
+		{
+			API::startSession();
+			if(!API::hasCode())
+			{
+				$authState = md5(rand());
+				$_SESSION['authState'] = $authState;
+				header('Location: ' . API::createAuthCodeURI(API::$redirectUri, implode(' ', $permissions), 'http://localhost' . $_SERVER['REQUEST_URI']));
+				exit;
+			}
+			else if(!API::hasAccessCode())
+			{
+				/*$ch = curl_init('https://accounts.google.com/o/oauth2/token');
+				curl_setopt($ch, CURLOPT_POST, true);
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+				//curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+				//curl_setopt($ch, CURLOPT_CAINFO, getcwd() . '\certificates\googleAccounts.crt');
+				
+				$postFields = array('code' => $_SESSION['authCode'], 'client_id' => API::$clientID, 
+									'client_secret' => API::$clientSecret, 
+									'redirect_uri' => 'http://localhost/yogstats/authCode.php', 'grant_type' => 'authorization_code');
+				
+				curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);
+				$postVal = curl_exec($ch);
+				echo 'curl done ' . curl_error($ch) . '<br>';
+				curl_close($ch);
+				echo $postVal;
+				*/
+				$GLOBALS['gClient'] = new Google_Client();
+				$GLOBALS['gClient']->setClientId(API::$clientID);
+				$GLOBALS['gClient']->setClientSecret(API::$clientSecret);
+				$GLOBALS['gClient']->setRedirectUri(API::$redirectUri);
+				$GLOBALS['gClient']->authenticate($_SESSION['authCode']);
+				$_SESSION['authAccessToken'] = $GLOBALS['gClient']->getAccessToken();
+					
+				return true;
+			}
+			
+			if(!isset($GLOBALS['gClient']))
+			{
+				$GLOBALS['gClient'] = new Google_Client();
+				$GLOBALS['gClient']->setClientId(API::$clientID);
+				$GLOBALS['gClient']->setClientSecret(API::$clientSecret);
+				$GLOBALS['gClient']->setRedirectUri(API::$redirectUri);
+				$GLOBALS['gClient']->setAccessToken($_SESSION['authAccessToken']);
+			}
+			
+			if(API::needsRefresh())
+			{
+				$GLOBALS['gClient']->refreshToken(json_decode($GLOBALS['gClient']->getAccessToken())->{'refresh_token'});
+				$_SESSION['authAccessToken'] = $GLOBALS['gClient']->getAccessToken();
+			}
+			
+			return true;
+		}
+			
+		public static function createAuthCodeURI($redirectURI, $permissions, $fromUrl)
+		{
+			return 'https://accounts.google.com/o/oauth2/auth?client_id=' . 
+			API::$clientID . 
+			'&response_type=code&access_type=offline&prompt=consent' . 
+			'&scope=' .
+			urlencode($permissions) .
+			'&redirect_uri=' . 
+			$redirectURI .
+			'&state=' . 
+			urlencode('security_token=' . $_SESSION['authState'] . '&from=' . $fromUrl);
+		}
+		
+		public static function getEmailAddress()
+		{
+			API::authenticate();
+			$service = new Google_Oauth2Service($GLOBALS['gClient']);
+			$info = $service->userinfo->get();
+			return $info['email'];
+		}
 	}
 ?>
