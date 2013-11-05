0b3ac225f77501156571dddb373b88493bf7186c
Thomas Williams <t.r.williamst@googlemail.com>
2013-11-05 10:03:01 +0000

    Sanitisation and validation
    Added more advanced sanitisation method, added a number validation
    method

--- PHP Lib/api.php
+++ PHP Lib/api.php
@@ -7,8 +7,43 @@
 	require_once 'PHP Lib/trackedChannel.php';
 	require_once 'PHP Lib/report.php';
 	
+	abstract class SanitiseType
+	{
+		public $String = 0;
+		public $Int = 1;
+		public $Float = 2;
+	}
+	
 	class Util
 	{
+		private static $SQLKeywords = array(
+		'ACCESSIBLE','ACTION','ADD','ALL','ALTER','ANALYZE', 'AND', 'AS', 'ASC', 'ASENSITIVE', 
+		'BEFORE', 'BETWEEN', 'BIGINT', 'BINARY', 'BIT', 'BLOB', 'BOTH', 'BY', 
+		'CALL', 'CASCADE', 'CASE', 'CHANGE', 'CHAR', 'CHARACTER', 'CHECK', 'COLLATE', 'COLUMN', 'CONDITION', 'CONSTRAINT', 'CONTINUE', 'CONVERT', 'CREATE','CROSS','CURRENT_DATA','CURRENT_TIME','CURRENT_TIMESTAMP','CURRENT_USER','CURSOR',
+		'DATABASE', 'DATABASES', 'DATE', 'DAY_HOUR', 'DAY_MICROSECOND', 'DAY_MINUTE', 'DAY_SECOND', 'DEC', 'DECIMAL', 'DECLARE','DEFAULT','DELAYED','DELETE','DESC','DESCRIBE','DETERMINISTIC','DISTINCT','DISTINCTROW', 'DIV', 'DOUBLE','DROP','DUAL',
+		'EACH', 'ELSE', 'ELSEIF', 'ENCLOSED', 'ENUM', 'ESCAPED', 'EXISTS', 'EXIT', 'EXPLAIN',
+		'FALSE', 'FETCH', 'FLOAT', 'FLOAT4', 'FLOAT8', 'FOR', 'FORCE', 'FOREIGN', 'FROM', 'FULLTEXT', 
+		'GRANT', 'GROUP', 'GENERAL',
+		'HAVING', 'HIGH_PRIORITY', 'HOUR_MICROSECOND','HOUR_MINUTE', 'HOUR_SECOND',
+		'IF', 'IGNORE', 'IGNORE_SERVER_IDS', 'IN', 'INDEX', 'INFILE', 'INNER', 'INOUT', 'INSENSITIVE','INSERT','INT','INT1','INT2','INT3','INT4','INT8','INTEGER','INTERVAL','INTO','IS','ITERATE',
+		'JOIN',
+		'KEY', 'KEYS', 'KILL',
+		'LEADING', 'LEAVE', 'LEFT', 'LIKE', 'LIMIT', 'LINEAR', 'LINES', 'LOAD', 'LOCALTIME', 'LOCALTIMESTAMP', 'LOCK', 'LONG', 'LONGBLOB', 'LONGTEXT', 'LOOP','LOW_PRIORITY', 
+		'MASTER_SSL_VERIFY_SERVER_CERT', 'MASTER_HEARTBEAT_PERIOD','MATCH' ,'MAXVALUE', 'MEDIUMBLOB', 'MEDIUMINT', 'MEDIUMTEXT', 'MIDDLEINT', 'MINUTE_MICROSECOND', 'MINUTE_SECOND', 'MOD', 'MODIFIES',
+		'NATURAL', 'NO', 'NOT', 'NO_WRITE_TO_BINLOG', 'NULL', 'NUMERIC',
+		'ON', 'OPTIMIZE', 'OPTION', 'OPTIONALLY', 'OR', 'ORDER', 'OUT', 'OUTER', 'OUTFILE', 
+		'PRECISION', 'PRIMARY', 'PROCEDURE', 'PURGE',
+		'RANGE', 'READ', 'READS', 'READ_WRITE', 'REAL', 'REFERENCES', 'REGEXP', 'RELEASE', 'RENAME' ,'REPEAT', 'REPLACE', 'REQUIRE', 'RESIGNAL', 'RESTRICT', 'RETURN', 'REVOKE', 'RIGHT', 'RLIKE',
+		'SCHEMA', 'SCHEMAS', 'SECOND_MICROSECOND', 'SELECT', 'SENSITIVE', 'SEPARATOR', 'SET', 'SHOW', 'SIGNAL', 'SLOW', 'SMALLINT', 'SPATIAL', 'SPECIFIC', 'SQL', 'SQLEXCEPTION', 'SQLSTATE', 'SQLWARNING', 'SQL_BIG_RESULT', 'SQL_CALC_FOUND_ROWS', 'SQL_SMALL_RESULT', 'SSL', 'STARTING', 'STRAIGHT_JOIN',
+		'TABLE', 'TERMINATED', 'TEXT', 'THEN', 'TIME', 'TIMESTAMP', 'TINYBLOB', 'TINYINT', 'TINYTEXT', 'TO', 'TRAILING', 'TRIGGER', 'TRUE',
+		'UNDO', 'UNION', 'UNIQUE', 'UNLOCK', 'UNSIGNED', 'UPDATE', 'USAGE', 'USE', 'USING', 'UTC_DATE', 'UTC_TIME', 'UTC_TIMESTAMP', 
+		'VALUES', 'VARBINARY', 'VARCHAR', 'VARCHARACTER', 'VARYING',
+		'WHEN', 'WHERE', 'WHILE', 'WITH', 'WRITE',
+		'XOR',
+		'YEAR_MONTH',
+		'ZEROFILL'
+		);
+		
 		public static function getUser()
 		{
 			if(!Auth::hasUser())
@@ -16,6 +51,72 @@
 				return null;
 			}
 			return $_SESSION['authUser'];
+		}
+		
+		public static function ValidateNumber($value, $min = PHP_INT_MAX+1, $max = PHP_INT_MAX)
+		{
+			if(!is_numeric($value)) 
+			{
+				return false;
+			}
+			if($value < $min)
+			{
+				return -1;
+			}
+			if($value > $max)
+			{
+				return 1;
+			}
+			return 0;
+		}
+		
+		public static function GetValidateErrorMessage($err, $min = PHP_INT_MAX+1, $max = PHP_INT_MAX)
+		{
+			if($err === false)
+			{
+				return 'Please enter a valid number';
+			}
+			if($err < 0)
+			{
+				return 'Please enter a number greater than or equal to ' . $min;
+			}
+			if($err > 0)
+			{
+				return 'Please enter a number less than or equal to ' . $max;
+			}
+			return '';
+		}
+		
+		public static function SQLSanitise($value, $type)
+		{
+			global $sqlDataSource;
+			switch($type) 
+			{
+				//String
+				case 0:
+					return $sqlDataSource->sanitise($value);
+				//Integer
+				case 1:
+					return intval($value);
+				case 2:
+					return floatval($value);
+				default:
+					throw new Exception('Unknown type ' . $type);
+			}
+		}
+		
+		private static function SQLStrip($value)
+		{
+			$isString = true;
+			$isBracket = 0;
+			
+			$currentWord = '';
+			$returnString = '';
+			
+			for($i = 0; $i < strlen($value); $i++)
+			{
+				
+			}
 		}
 	}
 	
