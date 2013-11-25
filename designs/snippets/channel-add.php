d17afe3d0d9a51a1c52e720eb4fe8b19893aa010
Thomas Williams <t.r.williamst@googlemail.com>
2013-11-25 13:29:00 +0000

    Moved PHP to channel-manager

--- designs/snippets/channel-add.php
+++ designs/snippets/channel-add.php
@@ -1,52 +1,5 @@
-<?php
-	require_once __DIR__ . '/../../PHP Lib/api.php';
-	
-	$authError = '';
-	try
-	{
-		Auth::authenticate();
-	}
-	catch(Exception $ex)
-	{
-		switch($ex->getCode())
-		{
-			default:
-				$authError = $ex->getCode() . ' ' . $ex-getMessage();
-		}
-	}
-	$createError = '';
-	if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['formID']))
-	{
-		switch($_POST['formID'])
-		{
-			case 'createChannel':
-				try
-				{
-					//TODO: Need to add permissions checks!
-					TrackedChannelDataHelper::addTrackedChannel($_POST['channelName']);
-				}
-				catch(Exception $ex)
-				{
-					switch($ex->getCode())
-					{
-						case 1:
-							$createError = $ex->getMessage();
-						break;
-						case 1062:
-							$createError = $_POST['channelName'] . ' already in tracked channels';
-						break;
-						default:
-						$createError = $ex->getCode() . ' ' . $ex->getMessage();
-					}
-				}
-			break;
-		}
-	}
-?>
-<script type="text/javascript" src="../../JS Lib/tools.js"> </script>
-<?php if($authError != '') echo '<p class="error-text">' . $authError . '</p>'; ?>
-<section class="channel-add">
-	<h2>Add a channel</h2>
+<section class="add">
+	<h3>Add a channel</h2>
 	<p>Add a channel here to have it's statistics be included in future reports.</p>
 	<form id = 'createChannel' onsubmit="postForm(this);" method="POST">
 		<label for="channel-name">YouTube Name:</label>
